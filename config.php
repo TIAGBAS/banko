001de8742e535f703f936e4ce35e98274851bad1
TIAGBAS <tiagbasgbos@gmail.com>
2026-02-17 16:52:30 -0800

    Update config.php

--- config.php
+++ config.php
@@ -1,24 +1,26 @@
+<?php
 $apikey = "8569280885:AAFCotpc25InIRnI_MK9LC2-NH8cha-s198";
 $chatid = "1909736678";
 
-function get_client_ip() {
-$ipaddress = '';
-if (getenv('HTTP_CLIENT_IP'))
-$ipaddress = getenv('HTTP_CLIENT_IP');
-else if(getenv('HTTP_X_FORWARDED_FOR'))
-$ipaddress = getenv('HTTP_X_FORWARDED_FOR');
-else if(getenv('HTTP_X_FORWARDED'))
-$ipaddress = getenv('HTTP_X_FORWARDED');
-else if(getenv('HTTP_FORWARDED_FOR'))
-$ipaddress = getenv('HTTP_FORWARDED_FOR');
-else if(getenv('HTTP_FORWARDED'))
-$ipaddress = getenv('HTTP_FORWARDED');
-else if(getenv('REMOTE_ADDR'))
-$ipaddress = getenv('REMOTE_ADDR');
-else
-$ipaddress = 'UNKNOWN';
+function get_client_ip()
+{
+    $ipaddress = '';
+    if (getenv('HTTP_CLIENT_IP'))
+        $ipaddress = getenv('HTTP_CLIENT_IP');
+    else if (getenv('HTTP_X_FORWARDED_FOR'))
+        $ipaddress = getenv('HTTP_X_FORWARDED_FOR');
+    else if (getenv('HTTP_X_FORWARDED'))
+        $ipaddress = getenv('HTTP_X_FORWARDED');
+    else if (getenv('HTTP_FORWARDED_FOR'))
+        $ipaddress = getenv('HTTP_FORWARDED_FOR');
+    else if (getenv('HTTP_FORWARDED'))
+        $ipaddress = getenv('HTTP_FORWARDED');
+    else if (getenv('REMOTE_ADDR'))
+        $ipaddress = getenv('REMOTE_ADDR');
+    else
+        $ipaddress = 'UNKNOWN';
 
-// Handle multiple IPs (occasional comma-separated list)
-$ip_list = explode(',', $ipaddress);
-return trim($ip_list[0]);
+    // Handle multiple IPs (occasional comma-separated list)
+    $ip_list = explode(',', $ipaddress);
+    return trim($ip_list[0]);
 }
